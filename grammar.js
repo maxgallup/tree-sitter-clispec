c7678c8b5f9525234fe0c3407b99a4360388f16f
maxgallup <maxgallup@pm.me>
2026-01-15 14:48:38 +0100

    feat: added cmd scopes

--- grammar.js
+++ grammar.js
@@ -11,40 +11,50 @@ export default grammar({
   name: "clispec",
 
   rules: {
-    source_file: ($) => repeat($._declaration),
-    _declaration: ($) => choice($.type_declaration),
+    source_file: ($) => repeat($.declaration),
 
     doc_comment: ($) => token(seq("///", /.*/)),
     line_comment: ($) => token(seq("//", /.*/)),
-    source_file: ($) => repeat($._declaration),
+
+    declaration: ($) =>
+      choice(
+        $.type_declaration,
+        $.opt_declaration,
+        $.arg_declaration,
+        $.cmd_declaration,
+        $.metadata_declaration,
+        $.doc_comment,
+        $.line_comment,
+      ),
+
+    metadata_declaration: ($) =>
+      seq(
+        "meta",
+        "{",
+        "\n",
+        seq("name:", field("name", $.string_literal), "\n"),
+        seq("description:", field("description", $.string_literal), "\n"),
+        seq("version:", field("version", $.string_literal), "\n"),
+        repeat($.declaration),
+        "}",
+        "\n",
+      ),
 
     named_identifier: ($) => token(/[A-Z][a-zA-Z0-9_]*/),
 
-    int_literal: ($) => token(/\d+/),
-    float_literal: ($) => token(/\d+\.\d+/),
-    string_literal: ($) => token(/\"[a-zA-Z0-9_]*\"/),
+    int_literal: ($) => token(/-?\d+/),
+    float_literal: ($) => token(/-?\d+\.\d+/),
+    string_literal: ($) => token(/\"(?:[^\"\\]|\\.)*\"/),
     bool_literal: ($) => token(choice("true", "false")),
 
-    type_declaration: ($) =>
-      seq(
-        "type",
-        field("type_id", $.named_identifier),
-        "=",
-        field(
-          "type_value",
-          choice($.type_alias, $.anonymous_enum_expression, $.nested_type),
-        ),
+    type_expression: ($) =>
+      field(
+        "expression",
+        choice($.named_reference, $.anonymous_enum_expression, $.nested_type),
       ),
+    nested_type: ($) => seq($.named_reference, "<", $.type_expression, ">"),
 
-    nested_type: ($) =>
-      seq(
-        field("type_id", $.named_identifier),
-        "<",
-        field("value", choice($.nested_type, $.named_identifier)),
-        ">",
-      ),
-
-    type_alias: ($) => field("type_id", $.named_identifier),
+    named_reference: ($) => field("id", $.named_identifier),
 
     anonymous_enum_expression: ($) =>
       choice($.integer_enum, $.float_enum, $.string_enum),
@@ -55,5 +65,43 @@ export default grammar({
 
     string_enum: ($) =>
       seq($.string_literal, repeat(seq("|", $.string_literal))),
+
+    type_declaration: ($) =>
+      seq("type", $.named_reference, "=", $.type_expression, "\n"),
+
+    requires_declaration: ($) => seq("requires:", $.type_expression),
+
+    excludes_declaration: ($) => seq("excludes:", $.type_expression),
+
+    opt_declaration: ($) =>
+      seq(
+        "opt",
+        $.named_reference,
+        "(",
+        seq($.string_literal, repeat(seq(",", $.string_literal))),
+        ")",
+        "=",
+        $.type_expression,
+        "\n",
+      ),
+
+    arg_declaration: ($) =>
+      seq(
+        "arg",
+        $.named_reference,
+        "(",
+        seq(
+          optional(field("start", $.int_literal)),
+          "..",
+          optional(field("end", $.int_literal)),
+        ),
+        ")",
+        "=",
+        $.type_expression,
+        "\n",
+      ),
+
+    cmd_declaration: ($) =>
+      seq("cmd", $.string_literal, "{", "\n", repeat($.declaration), "}", "\n"),
   },
 });
